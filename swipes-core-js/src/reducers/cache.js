077aa7983472e904c4d6da7013ce694435d94a66
Kasper Pihl Tornøe <kasper@pihl.it>
2018-09-24 19:34:56 +0300

    master: fixed counter to work with archived

--- swipes-core-js/src/reducers/cache.js
+++ swipes-core-js/src/reducers/cache.js
@@ -1,47 +1,45 @@
-import { fromJS } from 'immutable';
-import * as types from '../constants';
+import { fromJS } from "immutable";
+import * as types from "../constants";
 
 const initialState = fromJS({});
 
-export default function cacheReducer (state = initialState, action) {
-  const {
-    payload,
-    type,
-  } = action;
+export default function cacheReducer(state = initialState, action) {
+  const { payload, type } = action;
 
   switch (type) {
     case types.CACHE_SAVE: {
+      if (payload.data && payload.data.archived) return state;
       return state.setIn(payload.path, fromJS(payload.data));
     }
     case types.CACHE_SAVE_BATCH: {
-      payload.data.forEach((d) => {
+      payload.data.forEach(d => {
         state = state.setIn([...payload.path, d.id], fromJS(d));
-      })
+      });
       return state;
     }
     case types.CACHE_CLEAR: {
       return state.deleteIn(payload.path);
     }
-    case 'update': {
+    case "update": {
       payload.updates.forEach(({ type, data }) => {
-        const path = data.id.split('-');
+        const path = data.id.split("-");
         path[path.length - 1] = data.id;
         const currentVal = state.getIn([type, ...path]);
         // Ensure latest value wins!
-        if(!currentVal || currentVal.get('updated_at') < data.updated_at) {
-          if(data.archived) {
+        if (!currentVal || currentVal.get("updated_at") < data.updated_at) {
+          if (data.archived) {
             state = state.deleteIn([type, ...path]);
           } else {
             state = state.setIn([type, ...path], fromJS(data));
           }
         }
-      })
+      });
       return state;
     }
     case types.RESET_STATE: {
       return initialState;
     }
     default:
-      return state
+      return state;
   }
 }
--- swipes-core-js/src/reducers/counter.js
+++ swipes-core-js/src/reducers/counter.js
@@ -1,47 +1,52 @@
-import { fromJS } from 'immutable';
+import { fromJS } from "immutable";
 
 const initialState = fromJS({
   notification: [],
-  discussion: [],
+  discussion: []
 });
 
 export default function counterReducer(state = initialState, action) {
-  const {
-    type,
-    payload,
-  } = action;
+  const { type, payload } = action;
 
   switch (type) {
-    case 'init': {
+    case "init": {
       return fromJS(payload.counter);
     }
-    case 'me.clearCounter':
-    case 'me_cleared_counter': {
-      return state.set(`${payload.type}Ts`, payload.cleared_at)
-        .updateIn([payload.type],
-          cs => cs.filter(
-            c => c.get('read_at') > payload.cleared_at
-          )
+    case "me.clearCounter":
+    case "me_cleared_counter": {
+      return state
+        .set(`${payload.type}Ts`, payload.cleared_at)
+        .updateIn([payload.type], cs =>
+          cs.filter(c => c.get("read_at") > payload.cleared_at)
         );
     }
-    case 'update': {
+    case "update": {
       payload.updates.forEach(({ type, data }) => {
-        if(type !== 'discussion') return;
-        const subscription = data.followers.find(f => f.user_id === state.get('myId'));
-        if( 
-          data.last_comment_at > state.get('discussionTs') &&
+        if (type !== "discussion") return;
+        if (data.archived) {
+          return state.updateIn(["discussion"], d =>
+            d.filter(o => o.get("id") !== data.id)
+          );
+        }
+        const subscription = data.followers.find(
+          f => f.user_id === state.get("myId")
+        );
+
+        if (
+          data.last_comment_at > state.get("discussionTs") &&
           subscription &&
           (!subscription.read_at || subscription.read_at < data.last_comment_at)
         ) {
-          state = state.updateIn(['discussion'], 
-            d => d.filter(o => o.get('id') !== data.id)
-            .push(fromJS({
-              ts: data.last_comment_at,
-              id: data.id,
-            }))
-          )
+          state = state.updateIn(["discussion"], d =>
+            d.filter(o => o.get("id") !== data.id).push(
+              fromJS({
+                ts: data.last_comment_at,
+                id: data.id
+              })
+            )
+          );
         }
-      })
+      });
       return state;
     }
     default:
