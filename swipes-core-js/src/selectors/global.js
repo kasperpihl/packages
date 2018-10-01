85bef344bc57efc90152500fe0489a557241a076
Kasper Pihl Tornøe <kasper@pihl.it>
2018-10-01 16:52:25 +0300

    master: fixed global posts

--- swipes-core-js/src/selectors/global.js
+++ swipes-core-js/src/selectors/global.js
@@ -2,22 +2,18 @@ import { createSelector } from 'reselect';
 import createCachedSelector from 're-reselect';
 import { search as searchGoals } from './goals';
 import { search as searchMilestones } from './milestones';
-import { search as searchPosts } from './posts';
-
 
 const getSearchString = (state, props) => props.searchString;
 const getState = state => state;
 
 export const search = createCachedSelector(
-  [getSearchString, searchGoals, searchMilestones, searchPosts],
-  (searchString, foundGoals, foundMilestones, foundPosts) => {
+  [getSearchString, searchGoals, searchMilestones],
+  (searchString, foundGoals, foundMilestones) => {
     if (!searchString || !searchString.length) {
       return null;
     }
-    return foundGoals.concat(foundMilestones).concat(foundPosts).sort(
-      (a, b) => ((a.score > b.score) ? 1 : ((b.score > a.score) ? -1 : 0)),
-    );
-  },
-)(
-  (state, props) => getSearchString(state, props) || '',
-);
+    return foundGoals
+      .concat(foundMilestones)
+      .sort((a, b) => (a.score > b.score ? 1 : b.score > a.score ? -1 : 0));
+  }
+)((state, props) => getSearchString(state, props) || '');
