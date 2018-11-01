04a9ac4246cff7a5069262929f0f6c21d8665bf1
Kasper Pihl Tornoe <kasper@pihl.it>
2018-11-01 20:28:33 +0100

    v1: removing export for reducers

--- swipes-core-js/src/index.js
+++ swipes-core-js/src/index.js
@@ -1,9 +1,7 @@
-import * as reducers from './reducers';
-
 import Socket from './classes/Socket';
 
 const init = store => {
   window.socket = new Socket(store);
 };
 
-export { init, reducers };
+export { init };
