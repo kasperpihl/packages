11e36903496637fb77a3927a18f6b8f503f22294
Kasper Pihl Tornøe <kasper@pihl.it>
2019-01-30 10:11:50 +0200

    v2: fixed spelling mistake

--- swipes-core-js/src/redux/reducers.js
+++ swipes-core-js/src/redux/reducers.js
@@ -4,6 +4,4 @@ export { default as connection } from './connection/connectionReducer';
 export { default as counter } from './counter/counterReducer';
 export { default as global } from './global/globalReducer';
 export { default as me } from './me/meReducer';
-export {
-  default as organizations
-} from './organizations/organizationsReducers';
+export { default as organizations } from './organizations/organizationsReducer';
