865c2daa63875363579afab772716273b550f26f
Kasper Pihl Tornoe <kasper@pihl.it>
2019-02-28 09:18:39 +0100

    Fix completion weird bug with multilevel

--- core/src/utils/project/projectValidateStates.js
+++ core/src/utils/project/projectValidateStates.js
@@ -21,25 +21,27 @@ export default function projectValidateStates(
 
     const key = '' + indention; // Make sure key is a string
 
-    // set the allCompleted for this level, if it does not exist.
-    if (typeof allCompletedForLevel[key] === 'undefined') {
-      allCompletedForLevel[key] = !!completed;
-    }
-
     // If we hit a lower indention
     if (indention < currentIndention) {
       const childLevelKey = `${indention + 1}`; // Make sure key is a string
 
       // check if all of it's children are completed or incomplete
       const childCompleted = allCompletedForLevel[childLevelKey];
+
       // assign child value to the parent element if needed.
       if (childCompleted !== completed) {
         clientState = clientState.setIn(['completion', taskId], childCompleted);
+
         completed = childCompleted;
       }
 
       // Clean child level status, we won't need it now.
       delete allCompletedForLevel[childLevelKey];
+    }
+
+    // set the allCompleted for this level, if it does not exist.
+    if (typeof allCompletedForLevel[key] === 'undefined') {
+      allCompletedForLevel[key] = !!completed;
     }
 
     // Keep tracking if this level is all completed.
